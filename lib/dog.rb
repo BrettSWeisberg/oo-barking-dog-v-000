d5e4d6afd2c4181996a78310fa12943577faf340
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-17 17:43:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Dog
+  #body of the Dog class
+  def name=(name)
+    @name
+  end
+
+  def bark #instance method
+    puts "Woof!"
+  end
+end
+
+#fido = Dog.new
+#fido.bark
+
+#Methods = Behavoirs
+#variables = data
+#only allowed to call methods on obects
