55bd815b3905879f306f4e6f5050f1c869bc9427
Brett Weisberg <weisberg.brett@gmail.com>
2018-02-17 18:33:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,23 +1,15 @@
 class Dog
   #body of the Dog class
-  def initialize(name)
-     @name = name
-   end
-
-   def name
-     @name
-   end
 
    def name=(new_name)
      @name = new_name
    end
-  
-
+   
+   def name
+     @name
+   end
 
   def bark #instance method
     puts "Woof!"
   end
 end
-
-#fido = Dog.new
-#fido.bark
